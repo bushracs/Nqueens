d99a88f12dbb78c545fa47cb9bfd4c71f9142384
JanaRadwan <janakhaled@aucegypt.edu>
2023-02-14 18:14:20 +0200

    “message”

---
+++ function1.h
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#define N 8
+void printBoard(int board[N][N]);
+bool isValid(int board[N][N], int row, int col);
+bool solveNQueen(int board[N][N], int col);
+bool checkSolution();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Help      ^O Write Out ^W Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
