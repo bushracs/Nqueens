5702a28224fbb505d29b254cf3011524fa78a60e
JanaRadwan <124711319+JanaRadwan@users.noreply.github.com>
2023-02-14 20:13:40 +0200

    Update function1.h

--- function1.h
+++ function1.h
@@ -20,5 +20,3 @@ bool checkSolution();
 
 
 
-^G Help      ^O Write Out ^W Where Is  ^K Cut       ^T Execute   ^C Location
-^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
